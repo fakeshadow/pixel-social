d37d4fc187e6b84f803f5fd1dc354cd5dd7f4f90
fakeshadow <24548779@qq.com>
2019-08-15 08:43:34 -0400

    fix unimplemented default trait

--- src/model/psn.rs
+++ src/model/psn.rs
@@ -5,6 +5,12 @@ use crate::model::{
     errors::ResError,
     common::GetSelfId,
 };
+use psn_api_rs::{
+    models::{
+        EarnedTrophies,
+        PSNUserTrophySummary
+    }
+};
 
 pub type PSNUser = psn_api_rs::models::PSNUser;
 
@@ -12,6 +18,33 @@ pub type PSNUser = psn_api_rs::models::PSNUser;
 pub struct UserPSNProfile {
     pub id: u32,
     pub profile: PSNUser,
+}
+
+impl Default for UserPSNProfile {
+    fn default() -> UserPSNProfile {
+        UserPSNProfile {
+            id: 0,
+            profile: PSNUser {
+                online_id: "".to_string(),
+                np_id: "".to_string(),
+                region: "".to_string(),
+                avatar_url: "".to_string(),
+                about_me: "".to_string(),
+                languages_used: vec![],
+                plus: 0,
+                trophy_summary: PSNUserTrophySummary {
+                    level: 0,
+                    progress: 0,
+                    earned_trophies: EarnedTrophies {
+                        platinum: 0,
+                        gold: 0,
+                        silver: 0,
+                        bronze: 0
+                    }
+                }
+            }
+        }
+    }
 }
 
 
